b7dc87f7611e76235a3e20d58267952a592da52e
futao <1185172056@qq.com>
2018-11-12 21:58:34 +0800

    配置文件修改

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,18 +1,4 @@
 CREATE TABLE `futao_user` (
-  `id` varchar(32) NOT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  `password` varchar(300) DEFAULT NULL,
-  `age` varchar(3) DEFAULT NULL,
-  `mobile` varchar(11) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `address` varchar(300) DEFAULT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastModifyTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-
-
-CREATE TABLE `futao_user` (
   `id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
