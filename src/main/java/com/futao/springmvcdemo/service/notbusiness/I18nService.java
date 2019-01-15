86e4cc2c240248ef9ae707d18b0b7ec9704bd85c
futao <1185172056@qq.com>
2019-01-15 16:04:26 +0800

    1.<add>添加spring容器的获取 2.<update>优化国际化资源的获取方式

--- src/main/java/com/futao/springmvcdemo/service/notbusiness/I18nService.java
+++ src/main/java/com/futao/springmvcdemo/service/notbusiness/I18nService.java
@@ -4,28 +4,32 @@ import com.futao.springmvcdemo.foundation.LogicException;
 import com.futao.springmvcdemo.model.system.ErrorMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
+ * 国际化
+ *
  * @author futao
  * Created on 2019-01-15.
  */
-@Service
 public class I18nService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(I18nService.class);
 
-    @Resource
-    private MessageSource messageSource;
 
-    public String getMessage(String code) {
+    /**
+     * 获取消息
+     * 也可以使用下面的方式：
+     * Resource private MessageSource messageSource;
+     * return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+     *
+     * @param code
+     * @return
+     */
+    public static String getMessage(String code) {
         try {
-            return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+            return SpringUtils.getContext().getMessage(code, null, LocaleContextHolder.getLocale());
         } catch (NoSuchMessageException e) {
             LOGGER.error("获取国际化资源{}失败" + e.getMessage(), code, e);
             throw LogicException.le(ErrorMessage.I18N_RESOURCE_NOT_FOUND, new String[]{code});
